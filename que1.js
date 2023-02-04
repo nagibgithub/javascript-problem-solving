3c8a9585151c13216bae628f290591d7780300ed
nagibgithub <nmfuad.bd@gmail.com>
2023-02-04 12:50:53 +0600

    add files

---
+++ que1.js
@@ -0,0 +1,9 @@
+// ১. সিম্পল একটা ফাংশন লিখতে হবে। যেটার নাম হবে feetToInch এবং এই ফাংশন ইনপুট হিসেবে নিবে feet আর রিটার্ন করবে inch । অর্থাৎ এই ফাংশনকে কোন একটা ফিট বলে দিলে সে রিটার্ন হিসেবে বলে দিবে কত ইঞ্চি হয়।
+
+function feetToInch(feet) {
+    const inch = feet * 12;
+    return inch;
+}
+let inputFeet = 5;
+const result = feetToInch(inputFeet);
+console.log(result);
