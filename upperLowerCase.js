6c13c0c5643fac1b58f092e935f7d0219bdbb801
nagibgithub <nmfuad.bd@gmail.com>
2023-02-06 18:47:40 +0600

    upperCase lowerCase

--- upperLowerCase.js
+++ upperLowerCase.js
@@ -3,9 +3,9 @@ const inputName = "Fuad";
 function textToLowercase(textRegular) {
     return  textRegular.toLowerCase();
 }
-console.log(textToLowercase(inputName));
+// console.log(textToLowercase(inputName));
 
 function textToUpperCase(textRegular) {
     return  textRegular.toUpperCase();
 }
-console.log(textToUpperCase(inputName));
+// console.log(textToUpperCase(inputName));
